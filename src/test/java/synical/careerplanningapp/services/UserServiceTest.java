e7d8855fd3bf8c264b4b830b4302a02618ab342b
SynicalToo <87197716+synicalToo@users.noreply.github.com>
2023-10-17 23:22:53 +0800

    - re-written test cases

--- src/test/java/synical/careerplanningapp/services/UserServiceTest.java
+++ src/test/java/synical/careerplanningapp/services/UserServiceTest.java
@@ -22,66 +22,74 @@ class UserServiceTest {
     }
 
     @Test
-    void registerTest() {
-        // test register account long username
-        assertFalse(register("ThisUsernameIsWayTooLong", GLOBAL_PASSWORD, "member"), "Able to register account that exceeds username length!");
 
-        // test register account with invalid password requirements
-        assertFalse(register("username", "pass", "member"), "Able to register account that has password too short!");
-        assertFalse(register("username", "password", "member"), "Able to register account that has no uppercase, digits, symbols!");
-        assertFalse(register("username", "PASSWORD", "member"), "Able to register account that has no lowercase, digits, symbols!");
-        assertFalse(register("username", "Password", "member"), "Able to register account that has no digits or symbols!");
-        assertFalse(register("username", "Pass4word", "member"), "Able to register account that has no symbols!");
-
-        // test register account with valid requirements
-        assertTrue(register("tester", GLOBAL_PASSWORD, "member"), "Failed to register user account 1!");
-        assertTrue(register("Tester", "Th!sIsAValidPass4Word", "admin"), "Failed to register user account 2!");
-        assertTrue(register("OnlyTwentyCharacters", "Blu3Colors1?", "member"), "Failed to register user account 3!");
-
-        // test register account with duplicate entry member / admin
-        assertFalse(register("tester", GLOBAL_PASSWORD, "member"), "Able to register account with duplicate username 1!");
-        assertFalse(register("tester", GLOBAL_PASSWORD, "admin"), "Able to register account with duplicate username with different role 1!");
-
-        // test register account with duplicate entry member / admin with all caps
-        assertFalse(register("Tester", GLOBAL_PASSWORD, "member"), "Able to register account with duplicate username 2!");
-        assertFalse(register("Tester", GLOBAL_PASSWORD, "admin"), "Able to register account with duplicate username with different role 2!");
+    void registerLongUserNameTest() {
+        assertFalse(register("ThisUsernameIsWayTooLong", GLOBAL_PASSWORD, "member"));
     }
 
     @Test
-    void loginTest() {
-        // test log in for existing account
-        assertTrue(login("admin", GLOBAL_PASSWORD), "Failed to login to account!");
-
-        // test log in for non-existent account
-        assertFalse(login("non-existent", "non-existent"), "Able to login to non-existent account!");
-
-        // test log in with wrong password
-        assertFalse(login("admin", "wrong-password"), "Able to login to account with wrong password!");
+    void registerInvalidPasswordTest() {
+        assertFalse(register("username", "pass", "member"));
+        assertFalse(register("username", "password", "member"));
+        assertFalse(register("username", "PASSWORD", "member"));
+        assertFalse(register("username", "Password", "member"));
+        assertFalse(register("username", "Pass4word", "member"));
     }
 
     @Test
-    void deleteUserTest() {
-        // test delete existing account
-        assertTrue(deleteUser("tester"), "Failed to delete user account 1!");
-        assertTrue(deleteUser("Tester"), "Failed to delete user account 2!");
-        assertTrue(deleteUser("OnlyTwentyCharacters"), "Failed to delete user account 3!");
-
-        // test delete non-existent account
-        assertFalse(deleteUser("non-existent"), "Able to delete a non-existent account!");
+    void registerValidRequirementsTest() {
+        assertTrue(register("tester", GLOBAL_PASSWORD, "member"));
+        assertTrue(register("Tester", "Th!sIsAValidPass4Word", "admin"));
+        assertTrue(register("OnlyTwentyCharacters", "Blu3Colors1?", "member"));
     }
 
     @Test
-    void viewAccountDetailsTest() {
-        // test view account details for existing user
-        assertNotNull(viewAccountDetails("admin"), "Unable to view admin account details!");
+    void registerDuplicateEntriesTest() {
+        assertFalse(register("tester", GLOBAL_PASSWORD, "member"));
+        assertFalse(register("tester", GLOBAL_PASSWORD, "admin"));
+        assertFalse(register("Tester", GLOBAL_PASSWORD, "member"));
+        assertFalse(register("Tester", GLOBAL_PASSWORD, "admin"));
+    }
 
-        // test view account details for non-existent user
-        assertNull(viewAccountDetails("non-existent"), "Got account details for non-existent account!");
+    @Test
+    void loginExistingAccountTest() {
+        assertTrue(login("admin", GLOBAL_PASSWORD));
+    }
+
+    @Test
+    void loginNonExistingAccountTest() {
+        assertFalse(login("non-existent", "non-existent"));
+    }
+
+    @Test
+    void loginWrongPasswordTest() {
+        assertFalse(login("admin", "wrong-password"));
+    }
+
+    @Test
+    void viewAccountDetailsExistingUserTest() {
+        assertNotNull(viewAccountDetails("admin"));
+    }
+
+    @Test
+    void viewAccountDetailsNonExistingUserTest() {
+        assertNull(viewAccountDetails("non-existent"));
     }
 
     @Test
     void viewAllAccountDetailsTest() {
-        // test view all account details
-        assertNotNull(viewAllAccountDetails(), "Unable to view admin account details!");
+        assertNotNull(viewAllAccountDetails());
+    }
+
+    @Test
+    void deleteExistingUserTest() {
+        assertTrue(deleteUser("tester"), "Failed to delete user account 1!");
+        assertTrue(deleteUser("Tester"), "Failed to delete user account 2!");
+        assertTrue(deleteUser("OnlyTwentyCharacters"), "Failed to delete user account 3!");
+    }
+
+    @Test
+    void deleteNonExistingUserTest() {
+        assertFalse(deleteUser("non-existent"));
     }
 }
